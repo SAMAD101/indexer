5de758229d98bd4e90838e09bf61fb0eeb60dfcb
vivekpal1 <31vivekpal@gmail.com>
2024-09-06 04:31:16 +0700

    ft: wasmer, ipfs

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cargo build --release
+source .env
 
-RUST_LOG=info ./target/release/cypher-indexer
+if ! pgrep -x "ipfs" > /dev/null
+then
+    ipfs daemon &
+    IPFS_PID=$!
+fi
+
+wasmer run $WASM_MODULE_PATH --env-file .env
+
+if [ "$BACKUP_ENABLED" = true ] && [ "$BACKUP_IPFS_PIN" = true ]
+then
+    BACKUP_HASH=$(ipfs add -r $BACKUP_PATH | tail -n 1 | cut -d ' ' -f 2)
+    echo "Backup pinned to IPFS with hash: $BACKUP_HASH"
+fi
+
+if [ ! -z "$IPFS_PID" ]
+then
+    kill $IPFS_PID
+fi
